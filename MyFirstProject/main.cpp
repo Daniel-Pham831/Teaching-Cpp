40e9816ed3fe8564482621a9e298c38ecca088a9
Phúc Phạm <phamphuc0603@gmail.com>
2022-04-04 21:30:29 +0700

    Khoi tao chuong trinh

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	/*
+	if (dieu kien(a) nào đó) 
+	{
+		Nếu đúng chay những thứ trong đây
+	}else{
+		Nếu sai chay nhung thứ trong đây
+	}
+	*/
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
+// Bài 1
+/*
+viết chương trình nhập vào 2 số nguyên bất kì 10 và 4
+
+Kiểm tra xem 
+A có chia hết cho B hay ko
+
+nếu đúng thì in ra màn hình
+	Số A chia hết cho số B
+
+nếu sai thì in ra màn hình
+	Số A không chia hết cho số B
+*/
+
+//Bài 2
+/*
+viết chương trình nhập vào năm sinh bất kì của 1 người
+kiểm tra xem họ đã lớn hơn bằng 18 tuổi chưa
+
+Nếu đúng in ra
+	Đủ tuổi đi tù
+Nếu sai
+	Ko đủ tuối đi tù
+*/
+
+
+//Bài 3
+/*
+viết chương trình nhập vào 1 năm bất kì từ 0 -> 3000
+
+Kiểm tra xem năm đó có phải là năm nhuận hay ko
+
+Nếu đúng là năm nhuận
+	In ra -> Năm nhuận
+Nếu sai là năm thường
+	In ra -> Năm thường
+
+Biết rằng năm nhuận là năm chia hết cho 4
+*/
