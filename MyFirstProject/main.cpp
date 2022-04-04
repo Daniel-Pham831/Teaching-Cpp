32c403fcc76b0d7b37606aaeede4d0059ba66fda
Phúc Phạm <phamphuc0603@gmail.com>
2022-04-04 22:46:27 +0700

    Bài tập điểm 3 môn

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -8,3 +8,12 @@ int main() {
 	system("pause");
 	return 0;
 }
+/*
+	viết chương trình nhập vào điểm 3 môn học, Toán Lý Hóa
+	a) Xuất ra những môn dưới trung bình (điểm số < 5)
+	b) Xuất ra tổng trung bình 3 môn (lấy 3 môn cộng lại , chia 3)
+	c) Xuất ra học lực Tốt nếu(tổng trung bình 3 môn >= 8 )
+		học lực khá nếu(tổng trung bình 3mon >= 6.5 và <8)
+		học lực trung bình nếu(tổng trung bình 3 môn >= 5 và < 6)
+		học lực yếu nếu <5
+*/
