a729b6373d9f5332c61b8bb85c8f696dcf6fb998
Phúc Phạm <phamphuc0603@gmail.com>
2022-04-08 21:33:48 +0700

    bài tập for loop

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -4,49 +4,20 @@ using namespace std;
 int main() {
 
 
+	for (int i = 1; i<10; i=i+2) {
+		cout << i << endl;
+	}
+	
+
+
+
 	cout << endl;
 	system("pause");
 	return 0;
 }
-/*
-*	Bài 1
-	Viết chương trình nhập vào 3 số bất kì vd: 
-	a) Xuất ra theo giá trị TĂNG dần
-	b) Xuất ra theo giá trị GIẢM dần
-*/
+// In từ 100 -> 0;
+// 
+// In tất cả số lẻ từ 100 -> 0
+// In tất cả số chẵn từ 100 -> 0
 
-/*
-*	Bài 2
-	Nhập vào độ dài 3 cạnh của 1 tam giác
-	a) kiểm tra xem 3 cạnh đó có tạo nên 1 tam giác hợp lệ hay ko
-		Nếu đúng xuất ra -> Tam giác hợp lệ
-		Nếu sai -> Tam giác ko hợp lệ
 
-	b) Xuất ra diện tích của tam giác
-
-	c) Kiểm tra xem tam giác đó có phải là tam giác cân hay ko
-		Nếu có -> Tam giác cân
-		Nếu ko -> Tam giác ko cân
-
-	d) Kiểm tra xem tam giác đó có phải tam giác đều hay ko
-		Nếu có -> Tam giác đều
-		Nếu ko -> Tam giác ko đều
-*/
-
-/*
-	Bài 3
-	Viết chương trình nhập vào Tháng và Năm
-	a) Kiểm tra xem Tháng và Năm vừa nhập có hợp lệ hay ko? 
-		Có -> Hợp lệ (tự snghi xem thế nào là tháng, năm hợp lệ)
-		Ko -> Ko hợp lệ
-
-	b) Xuất ra số ngày có trong tháng đó 
-		vd1: Nhập vào Tháng 1, Năm 2000 
-		-> Xuất ra Tháng 1, Năm 2000 có 31 ngày
-
-		vd2: Nhập vào Tháng 2, Năm 2009 
-		-> Xuất ra Tháng 2, Năm 2000 có 29 ngày
-
-		vd3: Nhập vào Tháng 2, Năm 2000
-		-> Xuất ra Tháng 2, Năm 2000 có 28 ngày
-*/
