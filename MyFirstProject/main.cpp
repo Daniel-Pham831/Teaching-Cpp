ae479df6d5972c0e376afb294d8acf700350909b
Phúc Phạm <phamphuc0603@gmail.com>
2022-04-21 21:08:05 +0700

    bai tap cuối ngày 21/4

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -1,60 +1,44 @@
 ﻿#include <iostream>
 using namespace std;
 
-
 int main() {
 
- 
 
-    cout << endl;
-    system("pause");
-    return 0;
+
+
+
+	cout << endl;
+	system("pause");
+	return 0;
 }
 
 /*
-    Viết chương trình kêu người dùng nhập vào điểm 3 môn học
-    Toán Văn Anh , theo thứ tự
-    Tính trung bình  theo tiêu chí: đtb = (toan*3 + van*2 + anh)/6
+	Bài 1:
+	Viết 1 hàm xuất ra tên họ
+	tuổi
+	và thành phố đang sống
 
-    Xuất điểm trung bình ra
+	Bài 2:
+	Viết hàm tính tổng 2 số nguyên được truyền vào
+	Sau đó xuất ra màn hình  tổng 2 số đó
+	
+	Bài 3:
+	Viết 1 cái hàm trả về giá trị nhân 5 của 1 số nguyên
 
-    Hãy xếp loại học sinh đó biết rằng
-    Nếu điểm trung bình >= 8 và ko có môn nào dưới 6.5 -> Học sinh Giỏi
+	input 1 số nguyên
+	output 1 số nguyên nhân 5 của số vừa dc truyền vào
 
-    Nếu đtb nằm trong khoảng   7->8 (ko lấy số 8) và ko có môn nào dưới 5
-    -> Học sinh khá 
+	Bài 4:
+	Viết 1 hàm xuất kết quả học tập
+	input điểm 1 môn học
+	nếu >= 5 thì đậu
+	dưới 5 thì rớt
+	
+	*cố tình nhập đúng 0->10
 
-    Nếu đtb nằm trong khoảng 5->7 (ko lấy số 7)  và ko môn nào dưới 3.5
-    -> Học sinh trung bình
-
-    Nếu đtb < 5
-    -> Học sinh kém
-    
-    Ko dc nhập số âm, nếu nhập số âm thì kêu nhập lại
-    ko dc nhập số lớn hơn 10, nếu nhập sai bắt nhập lại
-
-
-    Bài 2:
-    Viết chương trình nhập vào 1 số nguyên N bất kì
-    Xuất ra tất cả các số chia hết cho N (trừ 1 vào chính nó)
-    Đếm xem có bao nhiu số
-
-    vd nhập vào 6
-    in ra
-    2 3
-
-    Có 2 số chia hết cho 6
-
-
-    vd nhập vào 10
-    in ra
-    2 5
-
-    Có 2 số chia hết cho 10
-
-
-    vd: nhập vào 7
-    in ra
-    -> Không có số nào chia hết cho nó
-        
+	Bài 5: 
+	Viết 1 hàm kiểm tra xem số vừa nhập có phải là số chẳn hay lẽ
+	input 1 số nguyên nào đó
+	
+	xuất ra số đó là chẵn hay lẽ
 */
